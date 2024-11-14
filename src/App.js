960f079e74d2843dc2884e88ad1f626dc0989802
l.gamezardashvili <l.gamezardashvili@rs.ge>
2024-11-14 16:17:02 +0400

    uppdate HashRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use Routes instead of Switch
+import { HashRouter  as Router, Route, Routes } from 'react-router-dom'; // Use Routes instead of Switch
 import AuthPage from './components/AuthPage/AuthPage';
 
 const App = () => {
   return (
     <Router>
-      <Routes> {/* This replaces the Switch component */}
-        <Route path="/" element={<AuthPage />} /> {/* Use the 'element' prop to pass components */}
-        {/* Add more routes as needed */}
+      <Routes> 
+        <Route path="/" element={<AuthPage />} /> 
       </Routes>
     </Router>
   );
