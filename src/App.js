eb311d777e1f54fa16f8e9d96d11be7e187f2a43
l.gamezardashvili <l.gamezardashvili@rs.ge>
2024-11-14 15:33:10 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AuthPage from './components/AuthPage/AuthPage';
 
 const App = () => {
   return (
-    <div>
-      <AuthPage />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={AuthPage} />
+        {/* Add more routes as needed */}
+      </Switch>
+    </Router>
   );
 };
 
