63e04389234a969cfb8df7ffc09c5530ec279751
l.gamezardashvili <l.gamezardashvili@rs.ge>
2024-11-14 15:42:17 +0400

    DSADAS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use Routes instead of Switch
 import AuthPage from './components/AuthPage/AuthPage';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={AuthPage} />
+      <Routes> {/* This replaces the Switch component */}
+        <Route path="/" element={<AuthPage />} /> {/* Use the 'element' prop to pass components */}
         {/* Add more routes as needed */}
-      </Switch>
+      </Routes>
     </Router>
   );
 };
